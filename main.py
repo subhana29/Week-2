b954e06f42b8767d2e46362e93d5b0aa46b28772
subhana29 <subhanakhan2929@gmail.com>
2025-04-09 14:55:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+data_dir = "https://www.kaggle.com/datasets/iamsouravbanerjee/animal-image-dataset-90-different-animals/data"
+
+datagen = ImageDataGenerator(
+    rescale=1.0/255.0,
+    validation_split=0.2,
+    rotation_range=30,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
+
+train_data = datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+
+val_data = datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+from tensorflow import keras
+
+base_model = keras.applications.MobileNetV2(
+    input_shape=(224, 224, 3),
+    include_top=False,
+    weights='imagenet'
+)
+base_model.trainable = False
+
+model = keras.Sequential([
+    base_model,
+    keras.layers.GlobalAveragePooling2D(),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(len(train_data.class_indices), activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
